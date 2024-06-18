82eb25cf2383c51d4eae0c8eff70fd7bf367ea9c
Vithusan19 <vithu0919@gmail.com>
2024-06-18 08:50:24 +0530

    ride

--- frontend/src/Admin/ViewRides.js
+++ frontend/src/Admin/ViewRides.js
@@ -10,10 +10,10 @@ const ViewRides = () => {
   const [isDeleteDialogVisible, setIsDeleteDialogVisible] = useState(false);
 
   const rides = [
-    { id: 1, name: "CST21012", start_place: "Jaffna", end_place: "Colombo", car_number: "CAR-1909", cost: "500", Date: "June 18, 2024", start_time: "12.00", end_time: "15.00", published_time: "8.00", user: { name: "Vithusan", email: "vithu@example.com", phone: "1234567890" } },
-    { id: 2, name: "CST21013", start_place: "Kandy", end_place: "Badulla", car_number: "CAR-0602", cost: "1200", Date: "June 19, 2024",start_time: "15.00", end_time: "17.00", published_time: "12.00", user: { name: "Shan", email: "Shan@example.com", phone: "1234567890" } },
-    { id: 3, name: "CST21019", start_place: "Vavuniya", end_place: "Colombo", car_number: "CAR-0682", cost: "1900",Date: "June 19, 2024", start_time: "18.00", end_time: "6.00", published_time: "12.00", user: { name: "Rajah", email: "Rajah@example.com", phone: "1234567890" } },
-    { id: 4, name: "CST21073", start_place: "Colombo", end_place: "Jaffna", car_number: "CAR-0689", cost: "1200", Date: "June 17, 2024",start_time: "15.00", end_time: "17.00", published_time: "15.00", user: { name: "Malur", email: "Malur@example.com", phone: "1234567890" } },
+    { id: 1, name: "CST21012", start_place: "Jaffna", end_place: "Colombo", car_number: "CAR-1909", cost: "500", Date: "June 18, 2024", start_time: "12.00", end_time: "15.00", published_time: "8.00", published_date: "June 17, 2024", user: { name: "Vithusan", email: "vithu@example.com", phone: "1234567890" } },
+    { id: 2, name: "CST21013", start_place: "Kandy", end_place: "Badulla", car_number: "CAR-0602", cost: "1200", Date: "June 19, 2024", start_time: "15.00", end_time: "17.00", published_time: "12.00", published_date: "June 16, 2024", user: { name: "Shan", email: "Shan@example.com", phone: "1234567890" } },
+    { id: 3, name: "CST21019", start_place: "Vavuniya", end_place: "Colombo", car_number: "CAR-0682", cost: "1900", Date: "June 19, 2024", start_time: "18.00", end_time: "6.00", published_time: "12.00", published_date: "June 11, 2024", user: { name: "Rajah", email: "Rajah@example.com", phone: "1234567890" } },
+    { id: 4, name: "CST21073", start_place: "Colombo", end_place: "Jaffna", car_number: "CAR-0689", cost: "1200", Date: "June 17, 2024", start_time: "15.00", end_time: "17.00", published_time: "15.00", published_date: "June 13, 2024", user: { name: "Malur", email: "Malur@example.com", phone: "1234567890" } },
   ];
 
   const handleUserSearchChange = (event) => {
@@ -44,22 +44,25 @@ const ViewRides = () => {
     setIsDeleteDialogVisible(false);
   };
 
-  const calculateElapsedTime = (publishedTime) => {
+  const calculateElapsedTime = (publishedTime, publishedDate) => {
     const currentTime = new Date();
+    const publishedDateTime = new Date(publishedDate);
     const [publishedHour, publishedMinute] = publishedTime.split('.').map(Number);
 
-    const publishedDate = new Date();
-    publishedDate.setHours(publishedHour);
-    publishedDate.setMinutes(publishedMinute);
+    publishedDateTime.setHours(publishedHour);
+    publishedDateTime.setMinutes(publishedMinute);
 
-    const elapsedTime = Math.floor((currentTime - publishedDate) / (1000 * 60)); 
-    const hours = Math.floor(elapsedTime / 60);
-    const minutes = elapsedTime % 60;
+    const elapsedTime = currentTime - publishedDateTime;
+    const elapsedMinutes = Math.floor(elapsedTime / (1000 * 60));
+    const elapsedHours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const elapsedDays = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
 
-    if (hours > 0) {
-      return `${hours} hours ago`;
+    if (elapsedDays > 0) {
+      return `${elapsedDays} days ago`;
+    } else if (elapsedHours > 0) {
+      return `${elapsedHours} hours ago`;
     } else {
-      return `${minutes} minutes ago`;
+      return `${elapsedMinutes} minutes ago`;
     }
   };
 
@@ -84,7 +87,23 @@ const ViewRides = () => {
     ride.user.name.toLowerCase().includes(searchTermUser.toLowerCase())
   );
 
-  const sortedRides = filteredRides.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+  const sortedRides = filteredRides.sort((a, b) => {
+    const currentDate = new Date();
+    const today = new Date(currentDate.setHours(0, 0, 0, 0));
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    const dateA = new Date(a.Date);
+    const dateB = new Date(b.Date);
+
+    if (dateA < today && dateB >= today) return 1; 
+    if (dateA >= today && dateB < today) return -1; 
+    if (dateA.toDateString() === today.toDateString() && dateB.toDateString() !== today.toDateString()) return -1; 
+    if (dateA.toDateString() !== today.toDateString() && dateB.toDateString() === today.toDateString()) return 1;
+    if (dateA.toDateString() === tomorrow.toDateString() && dateB.toDateString() !== tomorrow.toDateString()) return -1; 
+    if (dateA.toDateString() !== tomorrow.toDateString() && dateB.toDateString() === tomorrow.toDateString()) return 1;
+
+    return dateA - dateB;
+  });
 
   return (
     <>
@@ -92,12 +111,12 @@ const ViewRides = () => {
       <p>Here are the user details.</p>
 
       <div className="search-con">
-        <input
+      <input
           type="text"
-          placeholder="Search by username"
+          placeholder="Search by Destination"
           className="search-users"
-          value={searchTermUser}
-          onChange={handleUserSearchChange}
+          value={searchTermEndPlace}
+          onChange={handleEndPlaceSearchChange}
         />
         <input
           type="text"
@@ -106,12 +125,13 @@ const ViewRides = () => {
           value={searchTermStartPlace}
           onChange={handleStartPlaceSearchChange}
         />
-        <input
+     
+         <input
           type="text"
-          placeholder="Search by Destination"
+          placeholder="Search by username"
           className="search-users"
-          value={searchTermEndPlace}
-          onChange={handleEndPlaceSearchChange}
+          value={searchTermUser}
+          onChange={handleUserSearchChange}
         />
       </div>
 
@@ -146,10 +166,11 @@ const ViewRides = () => {
                 <img src={userIcon} alt="user" className="vr-userimg" />
                 <span className="vr-username">{ride.user.name}</span>
               </div>
-              <span className="vr-time">{calculateElapsedTime(ride.published_time)}</span>
+              <span className="vr-time">{calculateElapsedTime(ride.published_time, ride.published_date)}</span>
             </div>
           </div>
         ))}
+
       </div>
 
       {selectedRide && (
@@ -179,7 +200,7 @@ const ViewRides = () => {
 
       {isDeleteDialogVisible && (
         <div className="modal">
-          <div className="modal-content-delete">
+          <div className="modal-content">
             <h2>Confirm Deletion</h2>
             <p>Are you sure you want to delete this ride?</p>
             <div className="modal-content-delete-button">
