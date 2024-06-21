1bc5cc83798f904684a077f3a206def11a69a48b
nithushan21 <121450813+nithushan21@users.noreply.github.com>
2024-06-21 11:50:50 +0530

    Logout Component added

---
+++ frontend/src/components/LogoutConfirmation.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import '../styles/LogoutConfirmation.css';
+
+const LogoutConfirmation = ({ message, onConfirm, onCancel }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <p>{message}</p>
+        <div className="modal-actions">
+          <button className="modal-confirm" onClick={onConfirm}>Confirm</button>
+          <button className="modal-cancel" onClick={onCancel}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogoutConfirmation;
